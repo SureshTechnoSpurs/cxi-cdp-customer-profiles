35c7cbbb71359557159956c043813828e031c2e8
Suresh Baskaran <suresh.baskaran@technospurs.com>
2022-03-31 14:37:35 +0530

    Added validation for User Profile Search

---
+++ src/ClientWebAppService.UserProfile/Business/Validators/UserSearchModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using CXI.Contracts.UserProfile.Models;
+using FluentValidation;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ClientWebAppService.UserProfile.Business.Validators
+{
+    /// <summary>
+    /// Validator for <see cref="UsersearchModel"/>.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class UserSearchModelValidator : AbstractValidator<UserProfileSearchDto>
+    {
+        public UserSearchModelValidator()
+        {
+            RuleFor(x => x.PartnerId).NotEmpty();
+            RuleFor(x => x.Role).IsInEnum();
+        }
+    }
+}
