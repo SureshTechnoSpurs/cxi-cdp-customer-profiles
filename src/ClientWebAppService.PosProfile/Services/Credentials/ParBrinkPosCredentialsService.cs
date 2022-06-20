0f36d72a83981efbc9de9e2bfa19a3802c20b48d
Anton Samokhin <anton.samokhin@customerxi.com>
2022-06-20 13:19:37 +0300

    offboarding

--- src/ClientWebAppService.PosProfile/Services/Credentials/ParBrinkPosCredentialsService.cs
+++ src/ClientWebAppService.PosProfile/Services/Credentials/ParBrinkPosCredentialsService.cs
@@ -1,20 +1,43 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Azure;
 using ClientWebAppService.PosProfile.Models;
 using CXI.Common.Security.Secrets;
 using CXI.Contracts.PosProfile.Models.Create;
 using CXI.Contracts.PosProfile.Models.PosKeyReference;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace ClientWebAppService.PosProfile.Services.Credentials
 {
-    public class ParBrinkPosCredentialsService : IPosCredentialsService<PosCredentialsConfigurationParBrinkCreationDto>
+    public class ParBrinkPosCredentialsService : IPosCredentialsService<PosCredentialsConfigurationParBrinkCreationDto>, IPosCredentialsOffboardingService
     {
-        private readonly ISecretSetter _secretSetter;
+        private const string SecretNotFoundErrorCode = "SecretNotFound";
+        private const string OmnivorePosType = "parbrink";
 
-        public ParBrinkPosCredentialsService(ISecretSetter secretSetter)
+        private readonly ISecretSetter _secretSetter;
+        private readonly ISecretClient _secretClient;
+        private readonly ILogger<ParBrinkPosCredentialsService> _logger;
+
+        public ParBrinkPosCredentialsService(ISecretSetter secretSetter, ISecretClient secretClient, ILogger<ParBrinkPosCredentialsService> logger)
         {
             _secretSetter = secretSetter;
+            _secretClient = secretClient;
+            _logger = logger;
+        }
+
+        public async Task OffboardingProcess(string partnerId)
+        {
+            try
+            {
+                var posConfigurationSecretName = SecretExtensions.GetPosConfigurationSecretName(partnerId, OmnivorePosType);
+                await _secretClient.DeleteSecretAsync(posConfigurationSecretName);
+            }
+            catch (RequestFailedException ex) when (ex.ErrorCode == SecretNotFoundErrorCode)
+            {
+                _logger.LogError(ex, $"Secret was not found in key vault for ${partnerId}");
+                throw;
+            }
         }
 
         public Task<PosCredentialsConfiguration> OnboardingProcess(string partnerId, PosCredentialsConfigurationParBrinkCreationDto posConfigurations)
